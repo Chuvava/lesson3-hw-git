04696b26bda3545780d799200e3e7c342e78ccad
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2023-07-25 22:36:17 +0200

    initialized project

---
+++ src/test/java/com/demoqa/RegistrationFormTests.java
@@ -0,0 +1,25 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy="eager";
+    }
+
+    @Test
+    void test() {
+        open("https://ya.ru");
+    }
+}
