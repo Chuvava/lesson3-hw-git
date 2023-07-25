a81c1626ea98fc6e4421d1ed131760188a0c1153
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2023-07-25 22:47:19 +0200

    renamed test class

--- src/test/java/com/demoqa/GitTest.java
+++ src/test/java/com/demoqa/GitTest.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistrationFormTests {
+public class GitTest {
 
     @BeforeAll
     static void beforeAll() {
